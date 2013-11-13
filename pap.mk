7a16ce4420c115e02c80a31e18d56763de34a5f5
Rob Shannon <spirit2457@gmail.com>
2013-11-13 10:42:16 +0000

    Pap

--- pap.mk
+++ pap.mk
@@ -7,22 +7,19 @@ PRODUCT_RELEASE_NAME := endeavoru
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/pap/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/pap/config/nfc_enhanced.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/htc/endeavoru/full_endeavoru.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := endeavoru
 PRODUCT_NAME := pap_endeavoru
-PRODUCT_BRAND := htc
-PRODUCT_MODEL := EndeavorU
+PRODUCT_BRAND := htc_europe
 PRODUCT_MANUFACTURER := HTC
+PRODUCT_MODEL := One X
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=htc_europe \
     TARGET_DEVICE=endeavoru \
-    BUILD_FINGERPRINT="htc/htc_europe/endeavoru:4.4/JDQ39/231174.2:user/release-keys" \
+    BUILD_FINGERPRINT="htc/htc_europe/endeavoru:4.3/JRO03C/231174.2:user/release-keys" \
     PRIVATE_BUILD_DESC="4.18.401.2 CL231174 release-keys"
